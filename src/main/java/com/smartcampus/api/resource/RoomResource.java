7159531162b8664c4ed8e507e7e6b7c26fee2c20
MSM Shahla <maryamshahla465@gmail.com>
2026-04-20 16:45:13 +0530

    Added delete room endpoint

--- src/main/java/com/smartcampus/api/resource/RoomResource.java
+++ src/main/java/com/smartcampus/api/resource/RoomResource.java
@@ -5,6 +5,8 @@ import com.smartcampus.api.service.RoomService;
 
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
 import java.util.List;
 
 @Path("/api/v1/rooms")
@@ -14,26 +16,38 @@ public class RoomResource {
 
     private RoomService roomService = new RoomService();
 
+    // GET ALL ROOMS
     @GET
     public List<Room> getRooms() {
         return roomService.getAllRooms();
     }
 
+    // CREATE ROOM
     @POST
     public Room addRoom(Room room) {
         return roomService.addRoom(room);
     }
-    
+
+    // GET ROOM BY ID
     @GET
     @Path("/{id}")
     public Room getRoom(@PathParam("id") String id) {
         return roomService.getRoomById(id);
     }
-    
+
+    // DELETE ROOM 
+    @DELETE
+    @Path("/{id}")
+    public Response deleteRoom(@PathParam("id") String id) {
+        roomService.deleteRoom(id);
+        return Response.noContent().build();
+    }
+
+    // LINK SENSOR TO ROOM
     @POST
     @Path("/{roomId}/sensors/{sensorId}")
     public Room addSensorToRoom(@PathParam("roomId") String roomId,
-                                @PathParam("sensorId") String sensorId) {
+                               @PathParam("sensorId") String sensorId) {
         return roomService.addSensorToRoom(roomId, sensorId);
     }
 }
