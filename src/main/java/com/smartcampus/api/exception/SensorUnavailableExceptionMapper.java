4eb188fde2f91782887ea2b7e7f99d73e651534c
MSM Shahla <maryamshahla465@gmail.com>
2026-04-21 19:26:59 +0530

    Added error handling for unavailable sensors

--- src/main/java/com/smartcampus/api/exception/SensorUnavailableExceptionMapper.java
+++ src/main/java/com/smartcampus/api/exception/SensorUnavailableExceptionMapper.java
@@ -6,11 +6,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Provider
-public class LinkedResourceNotFoundExceptionMapper implements ExceptionMapper<LinkedResourceNotFoundException> {
+public class SensorUnavailableExceptionMapper implements ExceptionMapper<SensorUnavailableException> {
     @Override
-    public Response toResponse(LinkedResourceNotFoundException ex) {
+    public Response toResponse(SensorUnavailableException ex) {
         Map<String, String> error = new HashMap<>();
         error.put("error", ex.getMessage());
-        return Response.status(422).entity(error).build(); 
+        return Response.status(Response.Status.FORBIDDEN).entity(error).build(); // 403
     }
 }
