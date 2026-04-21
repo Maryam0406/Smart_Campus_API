f7824095adb9ec91814b32f020ec14fdd8fba8b6
MSM Shahla <maryamshahla465@gmail.com>
2026-04-21 19:38:03 +0530

    Fixed errors

--- src/main/java/com/smartcampus/api/resource/SensorReadingResource.java
+++ src/main/java/com/smartcampus/api/resource/SensorReadingResource.java
@@ -42,3 +42,4 @@ public class SensorReadingResource {
 
         return reading;
     }
+}
--- src/main/java/com/smartcampus/api/service/SensorService.java
+++ src/main/java/com/smartcampus/api/service/SensorService.java
@@ -58,6 +58,6 @@ public class SensorService {
             throw new SensorNotFoundException("Sensor not found");
         }
 
-        sensor.setValue(value);
+        sensor.setCurrentValue(value);
     }
 }
